835cb9080e7237c02ecc937b86fa7bb222276c81
karvasarniy.sa <karvasarniy.sa@intechs.by>
2021-08-09 12:00:26 +0300

    start

--- src/service/Service.jsx
+++ src/service/Service.jsx
@@ -23,8 +23,6 @@ export const request = (url) => {
     redirect: "follow",
   };
 
-  console.log("1");
-
   return fetch(`${defaultURL}${url}${language}`, requestOptions)
     .then((response) => response.json())
     .then((result) => result)
